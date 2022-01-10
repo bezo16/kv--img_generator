672b8e6bb6d5386e7040e4552171387d7dcea7ff
bezo16 <bezo16@protonmail.ch>
2022-01-10 11:01:15 +0100

    .

--- public/script.js
+++ public/script.js
@@ -10,24 +10,6 @@ for(let i=0; i < tempPr.length + 100; i+=100) {
     pr.push(tempPr.slice(i,i + 100))
 }
 
-// Example POST method implementation:
-async function postData(url = '', data) {
-    // Default options are marked with *
-    const response = await fetch(url, { 
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'no-cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
-  }
 
 
 let testing = 1
@@ -36,7 +18,19 @@ fetch('https://api.db-ip.com/v2/free/self')
 .then(res => res.json())
 .then(data => {
     console.log(data)
-    postData('https://72e5-95-102-48-209.ngrok.io', data)
+    fetch('https://72e5-95-102-48-209.ngrok.io', { 
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+      });
 //   .then(data2 => {
 //     console.log(data2); // JSON data parsed by `data.json()` call
 //   });
